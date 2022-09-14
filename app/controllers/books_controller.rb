eaa27f9bb0c57fecc1895f6e535bed64127b04ab
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-09-14 08:54:05 +0000

    ヘッダーのリンクを各ページと紐付けた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,24 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @booka = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
   end
   
   private
